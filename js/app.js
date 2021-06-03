402df3efc93a659cec90e5be6bc63633c3446c97
jude120 <melissjth@gmail.com>
2021-06-03 13:12:04 -0500

    mod ruta sw2

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 
 var url = window.location.href;
-var swLocstion ='twittorju/sw.js';
+var swLocstion ='/twittorju/sw.js';
 
 if (navigator.serviceWorker) {
     if (url.includes('localhost')) {
-        navigator.serviceWorker.register('/sw.js');
-    }else{
-        navigator.serviceWorker.register(swLocstion);
+       swLocstion ='/sw.js';
     }
+    navigator.serviceWorker.register(swLocstion);
+
 }
 
 // Referencias de jQuery
